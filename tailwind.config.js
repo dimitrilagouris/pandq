4822547cc7566f268efd42395a72a09388a0a57a
Dimitri Lagouris <dimitrilagouris@gmail.com>
2026-07-25 19:49:33 +1000

    add tailwind colours to safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,10 @@ export default {
     "./src/index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  safelist: [
+    'bg-red-500', 'bg-blue-500', 'bg-green-500', 'bg-yellow-500', 'bg-purple-500', 'bg-orange-500', 'bg-emerald-500', 'bg-amber-500', 'bg-pink-500', 'bg-teal-500',
+    'text-red-500', 'text-blue-500', 'text-green-500', 'text-yellow-500', 'text-purple-500', 'text-orange-500', 'text-emerald-500', 'text-amber-500', 'text-pink-500', 'text-teal-500'
+  ],
   theme: {
     extend: {
       fontFamily: {
